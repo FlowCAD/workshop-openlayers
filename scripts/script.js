41854e60e476507024b2b78203ac0d73df4c882b
FlowCAD <florian.cadoz@gmail.com>
2018-02-18 19:47:28 +0100

    Refacto & Drawing tool added

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,63 @@
+var source = new ol.source.Vector({
+    url: '/data/7day-M2.5.json',
+    format: new ol.format.GeoJSON()
+});
+
+var styleBasic = new ol.style.Style({
+    image: new ol.style.Circle({
+        radius: 5,
+        fill: new ol.style.Fill({
+            color: 'blue'
+        }),
+        stroke: new ol.style.Stroke({
+            color: 'white'
+        })
+    })
+});
+
+var styleSelected = new ol.style.Style({
+    image: new ol.style.Circle({
+        radius: 5,
+        fill: new ol.style.Fill({
+            color: 'cyan'
+        }),
+        stroke: new ol.style.Stroke({
+            color: 'white'
+        })
+    })
+});
+
+var draw = new ol.interaction.Draw({
+    source: source,
+    type: 'Point'
+});
+
+var select = new ol.interaction.Select({
+    style: styleSelected
+});
+
 var map = new ol.Map({
     target: 'map',
     layers: [
         new ol.layer.Tile({
             source: new ol.source.OSM()
         }),
-        new ol.layer.Image({
+        new ol.layer.Vector({
+            title: 'Earthquakes',
+            source: source,
+            style: styleBasic
+        })
+        // Image Vector
+        /*new ol.layer.Image({
             title: 'Earthquakes',
             source: new ol.source.ImageVector({
                 source: new ol.source.Vector({
                     url: '/data/7day-M2.5.json',
                     format: new ol.format.GeoJSON()
                 }),
-                style: new ol.style.Style({
-                    image: new ol.style.Circle({
-                        radius: 3,
-                        fill: new ol.style.Fill({
-                            color: 'red'
-                        })
-                    })
-                })
+                style: styleBasic
             })
-        })
+        })*/
     ],
     view: new ol.View({
         center: ol.proj.fromLonLat([1.4407, 43.5974]),
@@ -32,21 +69,7 @@ var map = new ol.Map({
             target: document.getElementById('scale-line')
         })
     ]),
-    interactions: ol.interaction.defaults().extend([
-        new ol.interaction.Select({
-            style: new ol.style.Style({
-                image: new ol.style.Circle({
-                    radius: 5,
-                    fill: new ol.style.Fill({
-                        color: 'cyan'
-                    }),
-                    stroke: new ol.style.Stroke({
-                        color: 'black'
-                    })
-                })
-            })
-        })
-    ])
+    interactions: ol.interaction.defaults().extend([draw, select])
 });
 
 map.on('singleclick', function (e) {
@@ -56,3 +79,9 @@ map.on('singleclick', function (e) {
     var infoElement = document.getElementById('info');
     infoElement.innerHTML = feature ? feature.get('title') : '';
 });
+
+draw.on('drawend', function(evt){
+    var feature = evt.feature;
+    var p = feature.getGeometry();
+    console.log(p.getCoordinates());
+})
