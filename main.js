20fadc920c2b1a030fe0ea0ef4be41773ec7fb95
FlowCAD <florian.cadoz@gmail.com>
2018-02-22 16:16:26 +0100

    Vector Layer added for showing user position

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ import View from 'ol/view';
 import TileLayer from 'ol/layer/tile';
 import XYZSource from 'ol/source/xyz';
 import proj from 'ol/proj';
+import VectorLayer from 'ol/layer/vector';
+import VectorSource from 'ol/source/vector';
+import Feature from 'ol/feature';
+import Point from 'ol/geom/point';
 
 const map = new Map({
   target: 'map-container',
@@ -16,11 +20,18 @@ const map = new Map({
   ],
   view: new View({
     center: [0, 0],
-    zoom: 13
+    zoom: 2
   })
 });
 
+const position = new VectorSource();
+const vector = new VectorLayer({
+  source: position
+});
+map.addLayer(vector);
+
 navigator.geolocation.getCurrentPosition(function(pos) {
     const coords = proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]);
-    map.getView().animate({center: coords, zoom: 13});
+    map.getView().animate({center: coords, zoom: 11});
+    position.addFeature(new Feature(new Point(coords)));
 });
